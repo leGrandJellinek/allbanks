afa8a311a5e377286e35d2ffec7abf5e37a22f86
Jellinek <jellinekbraus@gmail.com>
2022-08-30 14:03:12 +0500

    fixnav

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,12 +1,14 @@
 <template>
 <nav class="header__nav">
+    <div class="bg" @click="burger = false" :class="{active : burger && width <= 1160}"></div>
     <div class="header__nav-block">
         <div class="header__nav-logo">
             <a href="#!" class="logo"><img src="@/assets/images/logo.png" alt=""></a>
         </div>
-        <ul class="header__nav-menu">
+        <ul class="header__nav-menu" :class="{active : burger}">
             <li v-for="link in getMenuList" :key="link.id"><a href="#!" class="header__nav-link">{{link.title}}</a></li>
         </ul>
+        <button class="header__nav-burger" @click="burger = true" ><img src="@/assets/images/icons/menu-burger.svg" alt=""></button>
     </div>
     <div class="header__nav-signin">
         <a href="#!" class="header__nav-button">Войти</a>
@@ -17,11 +19,25 @@
 <script>
 
 export default {
+    data(){
+        return {
+            burger: false,
+            width: 0,
+        }
+    },
     computed: {
         getMenuList(){
             return this.$store.getters.getMenuList  
-        }
-    }
+        },
+    },
+    methods: {
+      updateWidth() {
+         this.width = window.innerWidth;
+        },
+    },
+    created() {
+        window.addEventListener('resize', this.updateWidth);
+    },
 }
 
 
